560352ee95f7d499746dd802e7fd1ce9a71f1186
codingk8 <codingk8@gmail.com>
2017-11-09 23:08:58 +0100

    Le commit avant de partir dormir

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,46 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const mysql = require('mysql');
+const config = require('../config.js');
 
-/* GET home page. */
+const connection = mysql.createConnection(config);
+
+connection.connect();
+
+/* GET Affichage de la page d'inscription avec le formulaire, qui fonctionne */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
+
+/* GET Affichage de la page de confirmation d'inscription ou de mise à jour, qui fonctionne */
+router.get('/confirmation', function(req, res, next) {
+  res.render('confirm');
+});
+
+/* GET Affichage de la page d'erreur 404, qui fonctionne */
+router.get('/erreur404', function(req, res, next) {
+  res.render('error');
+});
+
+/* GET Affichage de la page de login, qui fonctionne */
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
+/* GET Affichage de la page de visualisation */
+router.get('/wilderstogether', function(req, res, next) {
+  res.render('wilders');
+});
+
+/* GET Affichage de la page de mise à jour, qui donne une 404 pour l'instant */
+router.get('/miseajour', function(req, res, next) {
+  res.render('update');
+});
+
+/* GET /admin/create
+router.get('/create', function(req, res, next) {
+	// Formulaire de création d'article
+	res.render('admin-create');
+}); */
 
 module.exports = router;
