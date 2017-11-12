ee40e1a16d6eafe31eb2eb23d3bf4f7a8bfe5c2e
codingk8 <codingk8@gmail.com>
2017-11-12 22:20:21 +0100

    Le commit du dimanche soir

--- routes/index.js
+++ routes/index.js
@@ -8,43 +8,44 @@ const connection = mysql.createConnection(config);
 connection.connect();
 
 /* GET Affichage de la page d'inscription avec le formulaire, qui fonctionne */
-router.get('/', function(req, res, next) {
-  res.render('index');
+router.get('/', function (req, res, next) {
+	res.render('index');
 });
 
 /* GET Affichage de la page de confirmation d'inscription ou de mise à jour, qui fonctionne */
-router.get('/confirmation', function(req, res, next) {
-  res.render('confirm');
+router.get('/confirmation', function (req, res, next) {
+	res.render('confirm');
+});
+
+/* GET Affichage de la page de confirmation d'inscription ou de mise à jour, qui fonctionne */
+router.get('/confirmationmaj', function (req, res, next) {
+	res.render('confirmmaj');
 });
 
 /* GET Affichage de la page d'erreur 404, qui fonctionne */
-router.get('/erreur404', function(req, res, next) {
-  res.render('error');
+router.get('/erreur404', function (req, res, next) {
+	res.render('error');
 });
 
-/* GET Affichage de la page de login, qui fonctionne */
-router.get('/login', function(req, res, next) {
-  res.render('login');
+
+/* GET Affichage de la page de visualisation, qui fonctionne */
+router.get('/wilderstogether', function (req, res, next) {
+	res.render('wilders');
 });
 
-/* GET Affichage de la page de visualisation */
-router.get('/wilderstogether', function(req, res, next) {
-  res.render('wilders');
+/* GET Affichage de la page de mise à jour de mise à jour des infos personnelles qui fonctionne */
+router.get('/miseajour', function (req, res, next) {
+	res.render('update');
 });
 
-/* GET Affichage de la page de mise à jour, qui donne la page de mise à jour des infos personnelles */
-router.get('/miseajour', function(req, res, next) {
-  res.render('update');
+/* GET Affichage de la page de mise à jour pour le mot de passe oublié qui fonctionne */
+router.get('/motdepasseoublie', function (req, res, next) {
+	res.render('oubli');
 });
 
-/* GET Affichage de la page de mise à jour, qui donne la page de mise pour le mot de passe oublié */
-router.get('/motdepasseoublie', function(req, res, next) {
-  res.render('oubli');
-});
-
-/* POST Prise en compte des informations d'inscription */
-router.post('/', function(req, res, next) {
-	connection.query('INSERT INTO wilders (prenom, nom, email, motdepasse, naissance, adresse, codepostal, ville) VALUES (?, ?, ?, ?, ?, ?, ?, ?)', [req.body.prenom, req.body.nom, req.body.email, req.body.motdepasse, req.body.naissance, req.body.adresse, req.body.codepostal, req.body.ville], function(error) {
+/* POST Prise en compte des informations d'inscription qui fonctionne avec la bdd yeah */
+router.post('/', function (req, res, next) {
+	connection.query('INSERT INTO wilders (prenom, nom, email, motdepasse, naissance, adresse, codepostal, ville) VALUES (?, ?, ?, ?, ?, ?, ?, ?)', [req.body.prenom, req.body.nom, req.body.email, req.body.motdepasse, req.body.naissance, req.body.adresse, req.body.codepostal, req.body.ville], function (error) {
 		if (error) {
 			console.log(error);
 		} else {
@@ -53,7 +54,29 @@ router.post('/', function(req, res, next) {
 	});
 });
 
-/* GET Affichage de la page d'administration des données personnelles */
+/* GET Affichage de la page d'administration des données personnelles dans le formulaire de mise à jour
+router.post('/miseajour/idWilder(\\d+)', function (req, res, next) {
+	connection.query('select * from wilders where id= ?', [req.params.idWilder], function (error) {
+			if (error) {
+				console.log(error);
+			} else {
+				res.render('update'), {
+					wilders = results[0]
+				}
+			});
+	});
+}); */
+
+/* POST Prise en compte des informations de mise à jour
+router.post('/miseajour/:idWilder(\\d+)', function(req, res, next){
+	connection.query('UPDATE wilders SET prenom = ?, nom = ?, email = ?, motdepasse = ?, naissance = ?, adresse = ?, codepostal = ?, ville = ?, WHERE id = ?', [req.body.prenom, req.body.nom, req.body.email, req.body.motdepasse, req.body.naissance, req.body.adresse, req.body.codepostal, req.body.ville], function (error) {
+		if (error) {
+			console.log(error);
+		} else {
+			res.redirect('/confirmationmaj');
+		}
+	})
+}); */
 
 
 /* GET /admin/create
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,54 @@
-var express = require('express');
-var router = express.Router();
+/* Appel des modules */
+const express = require('express');
+const router = express.Router();
+const mysql = require('mysql');
+const config = require('../config.js');
 
-/* GET users listing. */
+/* Connection à la BDD */
+const connection = mysql.createConnection(config);
+
+connection.connect();
+
+/* GET Affichage de la page de login, qui fonctionne */
+router.get('/login', function (req, res, next) {
+	res.render('login');
+});
+
+/* POST Gestion du login */
+router.post('/login', function(req, res, next) {
+    let fail = '';
+	let email = req.body.login;
+	let password = req.body.password;
+	connection.query(`select * from users where email="${email}" and motdepasse="${password}";`, 
+        function (error, results, fields){
+        if (results.length==0) {
+			res.send('erreur');
+		} else {
+			req.session.connected=true;
+			res.redirect('/wilderstogether');
+		}
+	});
+});
+
+//* A quoi ça sert ce truc ? page log */
+router.get('/logged', function(req, res, next) {
+    if(req.session.connected){
+        res.redirect('/wilderstogether');
+    }else{
+        res.redirect('/login');
+    }
+});
+
+/* GET Process de déconnection Même question, à quoi ça sert ?*/
+router.get("/admin-logout", function(req, res, next) {
+    req.session.connected=false;
+    res.redirect('/login');
+  });
+
+
+/* GET users listing.
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
-});
+}); */
 
 module.exports = router;
